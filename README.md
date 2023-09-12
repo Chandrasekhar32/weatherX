c8ad7c310a7bc48c4d4286f0c0a3e2383c6f996c
Chandrasekhar3279 <chandra.maadala@cloudseed.io>
2023-09-12 12:24:22 +0530

    location setup added for ios and android

--- README.md
+++ README.md
@@ -14,3 +14,37 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+
+Geolocator::
+android::
+step-1:
+Add the following to your "gradle.properties" file:
+android.useAndroidX=true
+android.enableJetifier=true
+step:2
+Make sure you set the compileSdkVersion in your "android/app/build.gradle" file to 33:
+android {
+  compileSdkVersion 33
+}
+step:3
+On Android you'll need to add either the ACCESS_COARSE_LOCATION or the ACCESS_FINE_LOCATION permission to your Android Manifest. To do so open the AndroidManifest.xml file (located under android/app/src/main) and add one of the following two lines as direct children of the <manifest> tag 
+
+<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+<uses-permission android:name="android.permission.ACCESS_BACKGROUND_LOCATION" />
+
+ios::
+step1:
+On iOS you'll need to add the following entries to your Info.plist file (located under ios/Runner)
+
+<key>NSLocationWhenInUseUsageDescription</key>
+<string>This app needs access to location when open.</string>
+<key>NSLocationAlwaysUsageDescription</key>
+<string>This app needs access to location when in the background.</string>
+key>NSLocationTemporaryUsageDescriptionDictionary</key>
+<dict>
+  <key>YourPurposeKey</key>
+  <string>The example App requires temporary access to the device&apos;s precise location.</string>
+</dict>
--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -1,11 +1,11 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 
-class GlobalController extends GetxController{
-
+class GlobalController extends GetxController {
   //create various variables
   final RxBool _isLoading = true.obs;
-  final RxDouble _langitude = 0.0 .obs;
-  final RxDouble _latitude = 0.0 .obs;
+  final RxDouble _langitude = 0.0.obs;
+  final RxDouble _latitude = 0.0.obs;
 
 //instances for them to call
   RxBool checkLoading() => _isLoading;
@@ -15,19 +15,46 @@ class GlobalController extends GetxController{
   @override
   void onInit() {
     // TODO: implement onInit
-    if(_isLoading.isTrue){
+    if (_isLoading.isTrue) {
       getLocation();
-      }
+    }
     super.onInit();
   }
+
   @override
   void onClose() {
     // TODO: implement onClose
     super.onClose();
   }
 
-getLocation()async{
+  getLocation() async {
+    bool isServiceEnabled;
+    LocationPermission locationPermission;
 
-}
+    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isServiceEnabled) {
+      return Future.error('Location was not enabled');
+    }
 
+    //status of permission
+    locationPermission = await Geolocator.checkPermission();
+    if (locationPermission == LocationPermission.deniedForever) {
+      return Future.error('Location permission are denied forever');
+    } else if (locationPermission == LocationPermission.denied) {
+      //request permission
+      locationPermission = await Geolocator.requestPermission();
+      if (locationPermission == LocationPermission.denied) {
+        return Future.error('Location Permission is denied');
+      }
+    }
+    //getting the current position of the user
+    return await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high)
+        .then((value) {
+      //update latitude and longitude
+      _latitude.value = value.latitude;
+      _langitude.value = value.longitude;
+      _isLoading.value = false;
+    });
+  }
 }
